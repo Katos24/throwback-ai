9508109159ad27b172bea5fc106af124ca777280
alex <y83436545+Katos24@users.noreply.github.com>
2025-06-30 18:30:53 -0400

    Add background color test to _app.js to debug CSS loading

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "../lib/supabaseClient";
 
+
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";

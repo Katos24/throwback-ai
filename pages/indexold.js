521f3e0c705fe793d4b38fa6e53779381702f5f6
alex <y83436545+Katos24@users.noreply.github.com>
2025-07-31 10:51:25 -0400

    Refactor auth flow: split LoginForm & SignupForm, modernize UI, add OAuth handlers and redirects

---
+++ pages/indexold.js
@@ -0,0 +1,27 @@
+import HeroSection from '../components/home/HeroSection';
+import TopBanner from '../components/home/TopBanner';
+import ImageCompare from '../components/home/ImageCompareSection';
+import FeaturesSection from '../components/home/FeaturesSection';
+import HowItWorks from '../components/home/HowItWorksSection';
+import AiShowcase from '../components/home/AIShowcaseSection';
+import PricingSection from '../components/home/PricingSection';
+import CustomerSuccess from '../components/home/SuccessStories';
+import FAQSection from '../components/home/FAQSection';
+import CTASection from '../components/home/CTASection';
+
+export default function Home() {
+  return (
+    <main>
+     <HeroSection />
+     <TopBanner />
+      <ImageCompare />
+      <FeaturesSection />
+      <HowItWorks />
+      <AiShowcase />
+      <PricingSection />
+      <CustomerSuccess />
+      <FAQSection />
+      <CTASection />
+    </main>
+  );
+}

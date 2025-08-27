260a1763ac7e7650042512ffd8cdb4584f2f3e26
alex <y83436545+Katos24@users.noreply.github.com>
2025-08-27 13:49:32 -0400

    Add Google Analytics gtag.js to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "../lib/supabaseClient";
+import Script from "next/script";
 
 import "../styles/globals.css";
 import Header from "../components/Header";
@@ -11,13 +12,25 @@ import { Toaster } from "react-hot-toast"; // ✅ Toast UI
 export default function MyApp({ Component, pageProps }) {
   const [showMenu, setShowMenu] = useState(false);
 
-  // 🔥 Removed credits override for guest users
-
   return (
     <SessionContextProvider
       supabaseClient={supabase}
       initialSession={pageProps.initialSession}
     >
+      {/* Google Tag */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JCY37JB8YP"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-JCY37JB8YP');
+        `}
+      </Script>
+
       <Header showMenu={showMenu} setShowMenu={setShowMenu} />
 
       <main>
